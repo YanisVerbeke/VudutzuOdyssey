38d5f6ad5f726ea97050d3a962a3ce4df4c98652
Unknown <antonin.pulby@edu.itescia.fr>
2019-11-19 13:52:58 +0100

    Des sprites et animation et coliiders 2D

--- VudutzuOdyssey/Assets/Scripts/PlayerFollow.cs
+++ VudutzuOdyssey/Assets/Scripts/PlayerFollow.cs
@@ -20,25 +20,39 @@ public class PlayerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ( Player.transform.position.x > MinX && Player.transform.position.x < MaxX )
+        if ( (Player.transform.position.x > MinX && Player.transform.position.x < MaxX) && ( Player.transform.position.y > MinY && Player.transform.position.y < MaxY ))
         {
             Deplacement = new Vector3(Player.transform.position.x, Player.transform.position.y, 0) + offset;
 
-        } else if( Player.transform.position.x <= MinX){
-            Deplacement = new Vector3(MinX, Player.transform.position.y, 0) + offset;
-            Debug.Log("x < minX");
-            transform.position = Deplacement;
-            return;
-        }
-        else if( Player.transform.position.x >= MaxX - 1){
-            Deplacement = new Vector3(MaxX, Player.transform.position.y , 0) + offset ;
-            Debug.Log("x > maxX");
-            transform.position = Deplacement;
-            return;
+        } else {
+            if ( Player.transform.position.x <= MinX && Player.transform.position.y <= MinY){
+                Deplacement = new Vector3(MinX, MinY, 0) + offset;
+            }
+            else if( Player.transform.position.x >= MaxX && Player.transform.position.y >= MaxY ){
+                Deplacement = new Vector3(MaxX, MaxY , 0) + offset ;
+            }
+            else if( Player.transform.position.x <= MinX && Player.transform.position.y >= MaxY ){
+                Deplacement = new Vector3(MinX, MaxY , 0) + offset ;
+            }
+            else if( Player.transform.position.x >= MaxX && Player.transform.position.y <= MinY ){
+                Deplacement = new Vector3(MaxX, MinY , 0) + offset ;
+            }
+            else if ( Player.transform.position.x <= MinX){
+                Deplacement = new Vector3( MinX ,Player.transform.position.y, 0) + offset;
+            }
+            else if( Player.transform.position.x >= MaxX){
+                Deplacement = new Vector3( MaxX ,Player.transform.position.y, 0) + offset;
+            }
+            else if(Player.transform.position.y >= MaxY ){
+                Deplacement = new Vector3(Player.transform.position.x, MaxY, 0) + offset;
+            }
+            else if(Player.transform.position.y <= MinY ){
+                Deplacement = new Vector3(Player.transform.position.x, MinY, 0) + offset;
+            }
         }
     
 
-
+        /*
         if ( Player.transform.position.y > MinY && Player.transform.position.y < MaxY )
         {
             Deplacement = new Vector3(Player.transform.position.x, Player.transform.position.y, 0) + offset;
@@ -53,6 +67,7 @@ public class PlayerFollow : MonoBehaviour
             transform.position = Deplacement;
             return;
         }
+        */
         transform.position = Deplacement;
     }
 }
