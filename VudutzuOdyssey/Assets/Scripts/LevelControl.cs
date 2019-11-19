d35ff326a33815fc15e447fa6e267a9aad6f6ad6
Unknown <thibaud.roussel@edu.itescia.fr>
2019-11-19 12:04:25 +0100

    Transition Map

---
+++ VudutzuOdyssey/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    //public int index;
+    public string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Perso")
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
