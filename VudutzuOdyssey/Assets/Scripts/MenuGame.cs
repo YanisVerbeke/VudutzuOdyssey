e719f3a70fc643ebd0051bb57757b075c9aa974b
Unknown <thibaud.roussel@edu.itescia.fr>
2019-11-19 16:47:50 +0100

    Create Menu UI

---
+++ VudutzuOdyssey/Assets/Scripts/MenuGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuGame : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    
+
+    public Button yourButton;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene("VillageScene");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+        Debug.Log("You have clicked the button!");
+    }
+}
