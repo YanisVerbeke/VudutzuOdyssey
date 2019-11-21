33642474c92370f3c1c66fead5849d6c0b6fe5d8
Lowik BOURGEOISAT <kiwol95@gmail.com>
2019-11-21 15:31:44 +0100

    UI finished for FightScene

---
+++ VudutzuOdyssey/Assets/Scripts/CanvaOpener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CanvaOpener : MonoBehaviour
+{
+    public GameObject Canva;
+
+    public void OpenCanva()
+    {
+        if(Canva != null)
+        {
+
+            Canva.SetActive(true);
+        }
+    }
+
+    public void CloseCanva()
+    {
+        Canva.SetActive(false);
+    }
+}
